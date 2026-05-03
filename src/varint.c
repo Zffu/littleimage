ae7a3ca7bb66109102eae9f58979f1e419d394a0
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-03 08:23:55 +0200

    feat: added varint impl

---
+++ src/varint.c
@@ -0,0 +1,41 @@
+#include "../include/internals.h"
+
+#define SEGMENT_BITS 0x7F
+#define CONTINUE_BIT 0x80
+
+size_t read_varint(uint8_t *buff) {
+  size_t value = 0;
+  int position = 0;
+  uint8_t current_byte;
+  size_t ind = 0;
+
+  while (1) {
+    current_byte = buff[ind++];
+    value |= (current_byte & SEGMENT_BITS);
+
+    if ((current_byte & CONTINUE_BIT) == 0)
+      break;
+
+    position += 7;
+
+    if (position >= sizeof(size_t))
+      return value;
+  }
+
+  return value;
+}
+
+void write_varint(uint8_t *buff, size_t value) {
+  size_t ind = 0;
+
+  while (1) {
+    if ((value & ~SEGMENT_BITS) == 0) {
+      buff[ind] = value;
+      return;
+    }
+
+    buff[ind++] = ((value & SEGMENT_BITS) | CONTINUE_BIT);
+
+    value >>= 7;
+  }
+}
